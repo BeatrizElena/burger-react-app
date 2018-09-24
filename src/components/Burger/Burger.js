4ce09ac846b07d274bc16e0adfff90b0b4db5eda
beatrizelena <ortizwebdevelopment@gmail.com>
2018-09-23 21:17:15 -0400

    Output burger ingredients dynamically Lots of comments to explain the code

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,31 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    // Use JS's Object.keys to extract the value of the ingredients in 
+    // the object defined in BurgerBuilder.js, then use map() to do something 
+    // with each ingredient (igKey) returned by the map function
+    // the transformed ingredients const will then go in the return statement of
+    // the const burger
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            // for each ingredient returned by map(), use the Array() method to 
+            // transform it into a new array of length given
+            // e.g. In our ingredient 'cheese' has a value of 2, the arrays will have a length of 2
+            // that value is found with the argument inside the Array method: props.ingredients[igKey]
+            // then, inside the outer map() function, we execute map() again on the new array created
+            // for the inner map() method, its 1st argument is an underscore _ because
+            // we don't care what ingredient is returned (cheese, bacon, anything)
+            // we do care how many of each ingredient, i represents that
+            // the inner map() returns the ingredient from the outer array (igKey) PLUS i, which
+            // represents the value of that key (igKey)
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
     return (
         <div className={classes.Burger}>
          <BurgerIngredient type="bread-top" />
-         <BurgerIngredient type="cheese" />
-         <BurgerIngredient type="meat" />
+         {transformedIngredients}
          <BurgerIngredient type="bread-bottom" />
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,30 @@ import Aux from  '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    // old constructor syntax
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    // more modern syntax to replace the above code:
+    // (be sure to use the same ingredients used in the switch cases in BurgerIngredient.js)
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    // goal from here is to pass these burger array to our BurgerBuilder
+    // by usig {this.state.ingredients}
+    // then pass it to our Burger.js component (will need to convert the ingredient
+    // object to an array when we get to the Burger.js file)
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         )
